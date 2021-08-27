a2c9e78d5e83d5fb17ecb3c0ea9cecfe7a76e3e6
Thiago Miziara <thiagonmiziara@gmail.com>
2021-08-27 20:11:43 -0300

    add config styled-components and jest

---
+++ src/components/Main/styles.ts
@@ -0,0 +1,32 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  width: 100%;
+  height: 100%;
+  padding: 3rem;
+  background-color: #06092b;
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+`
+export const Logo = styled.img`
+  width: 25rem;
+  margin-bottom: 2rem;
+`
+
+export const Title = styled.h1`
+  font-size: 2.5rem;
+`
+
+export const Description = styled.h2`
+  font-size: 2rem;
+  font-weight: 400;
+`
+
+export const Illustration = styled.img`
+  margin-top: 3rem;
+  width: min(30rem, 100%);
+`
